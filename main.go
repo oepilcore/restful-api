ce486c9a37bd1b80b9f3b6b716758a7432987dae
Muhammad Aji Prasetyo <75011718+oepilcore@users.noreply.github.com>
2024-02-28 08:17:36 +0700

    Add files via upload

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+
+		c.JSON(http.StatusOK, gin.H{
+			"name":    "api server 1",
+			"version": "v1.0",
+		})
+	})
+	r.Run()
+}
